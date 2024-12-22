6512ed5a3c0f6406fa2b1619da4af2e830c550c1
Benedict Anemba <anembaben@gmail.com>
2024-12-22 21:17:37 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(['auth','verified']);
+
+Route::view('/profile/edit', 'profile.edit')->middleware('auth');
+
+Route::view('/profile/password', 'profile.password')->middleware('auth');
+
+Route::middleware(['admin.only'])->group(function () {
+    // Define your admin panel routes here
+});
+
